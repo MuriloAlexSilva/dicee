9d89996ba97f7a91f80718d5804b9ba0a0b7a689
Murilo Silva <murilo.alex.silva@gmail.com>
2020-10-23 10:22:09 -0300

    atualização de estado

--- dicee/lib/dicee.dart
+++ dicee/lib/dicee.dart
@@ -1,14 +1,64 @@
 import 'package:flutter/material.dart';
 
-class Dicee extends StatelessWidget {
+class Dicee extends StatefulWidget {
+  @override
+  _DiceeState createState() => _DiceeState();
+}
+
+class _DiceeState extends State<Dicee> {
+  int rightButton = 1;
+
+  int leftButton = 1;
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Column(
-      children: [
-        Expanded(flex: 1, child: Image.asset("assets/images/dice1.png")),
-        Expanded(flex: 1, child: Image.asset("assets/images/dice2.png")),
-      ],
-    ));
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: Text("Dicee"),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Row(
+            children: [
+              Expanded(
+                flex: 1,
+                child: FlatButton(
+                  onPressed: () {
+                    setState(() {
+                      if (leftButton < 6) {
+                        leftButton++;
+                      } else {
+                        leftButton = 1;
+                      }
+                    });
+                  },
+                  child: Image.asset("assets/images/dice$leftButton.png"),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: FlatButton(
+                  onPressed: () {
+                    setState(() {
+                      if (rightButton < 6) {
+                        rightButton++;
+                        print(rightButton);
+                      } else {
+                        rightButton = 1;
+                      }
+                    });
+                  },
+                  child: Image.asset("assets/images/dice$rightButton.png"),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
