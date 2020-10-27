722fecc0d149c8543c7721e747334eeb05e1f935
Murilo Silva <murilo.alex.silva@gmail.com>
2020-10-27 09:42:54 -0300

    app concluido

--- dicee/lib/dicee.dart
+++ dicee/lib/dicee.dart
@@ -9,8 +9,21 @@ class Dicee extends StatefulWidget {
 
 class _DiceeState extends State<Dicee> {
   int rightButton = 1;
-
   int leftButton = 1;
+
+  void changeDice() {
+    setState(() {
+      leftButton = Random().nextInt(6) + 1;
+      rightButton = Random().nextInt(6) + 1;
+      /*Para utilizar esta função deverá conter o dart: math
+        colocando o random().nextInt() ele ira inserir valores aleatorios até 
+        o numero inserido dentro de nextInt "significa proximo inteiro", porém 
+        ele considera o zero, indo assim neste caso acima de 0 até 5, que daria 6 numeros,
+        sendo assim colocamos o "+1", para quando der zero ele ficar 1 e assim sempre irá
+        somar 1
+        */
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,17 +43,7 @@ class _DiceeState extends State<Dicee> {
                 flex: 1,
                 child: FlatButton(
                   onPressed: () {
-                    setState(() {
-                      leftButton = Random().nextInt(6) + 1;
-                      rightButton = Random().nextInt(6) + 1;
-                      /*Para utilizar esta função deverá conter o dart: math
-                      colocando o random().nextInt() ele ira inserir valores aleatorios até 
-                      o numero inserido dentro de nextInt "significa proximo inteiro", porém 
-                      ele considera o zero, indo assim neste caso acima de 0 até 5, que daria 6 numeros,
-                      sendo assim colocamos o "+1", para quando der zero ele ficar 1 e assim sempre irá
-                      somar 1
-                      */
-                    });
+                    changeDice();
                   },
                   child: Image.asset("assets/images/dice$leftButton.png"),
                 ),
@@ -49,17 +52,7 @@ class _DiceeState extends State<Dicee> {
                 flex: 1,
                 child: FlatButton(
                   onPressed: () {
-                    setState(() {
-                      rightButton = Random().nextInt(6) + 1;
-                      leftButton = Random().nextInt(6) + 1;
-                      /*Para utilizar esta função deverá conter o dart: math
-                      colocando o random().nextInt() ele ira inserir valores aleatorios até 
-                      o numero inserido dentro de nextInt "significa proximo inteiro", porém 
-                      ele considera o zero, indo assim neste caso acima de 0 até 5, que daria 6 numeros,
-                      sendo assim colocamos o "+1", para quando der zero ele ficar 1 e assim sempre irá
-                      somar 1
-                      */
-                    });
+                    changeDice();
                   },
                   child: Image.asset("assets/images/dice$rightButton.png"),
                 ),
